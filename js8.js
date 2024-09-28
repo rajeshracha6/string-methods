4f01883715d3d29ed470e14b71d6c3fa129811c2
rajeshracha6 <rajeshracha752@gmail.com>
2024-09-28 12:26:43 +0530

    done

---
+++ js8.js
@@ -0,0 +1,188 @@
+// let str ="hello"
+// let str2=str.charAt(0).toUpperCase()+str.slice(1);
+// console.log(str2) 
+
+// let str= "hello";
+// res=""
+// // str = str[0].toUpperCase()
+// for(i=0;i<str.length;i++){
+//     if(i==0)
+//     res += str[i].toUpperCase()
+//     else
+//     res+=str[i]
+//     }
+// console.log(res)
+
+// let rajesh="world";
+// let rajesh2=rajesh.charAt(0).toUpperCase()+rajesh.slice(1);
+// console.log(rajesh2)
+ 
+// function removeVowels(inputString) {
+//     const vowels = "aeiouAEIOU";
+//     let result = "";
+
+//     for (let i = 0; i < inputString.length; i++) {
+//         if (!vowels.includes(inputString[i])) {
+//             result += inputString[i];
+//         }
+//     }
+
+//     return result;
+// }
+
+// console.log(removeVowels("hello")); // Output: "hll"
+// console.log(removeVowels("world")); // Output: "wrld"
+
+// function removeVowels(inputString){
+//     return inputString.replace(/[aeiouAEIOU]/g, "");
+        
+// }
+// console.log(removeVowels("hello")); // Output: "hll"
+// console.log(removeVowels("world")); // Output: "wrld"
+
+// string at:
+
+// .at method will allows both nagitive and positive indeess 
+
+// let str="something";
+// x=str.at(8);
+// console.log(x)
+
+// charAt:
+
+// charAt method will allow only positive  dont allows nagitive vallues 
+
+// let x = "something".charAt(2);
+// console.log(x);
+
+// Length: Returns the length of a string.
+
+// let text ="bramhachari"
+// console.log( text.length);
+
+// toUpperCase() / toLowerCase(): Converts the string to upper/lower case.
+
+// let x= "mounika";
+// console.log(x.toUpperCase());
+
+// charCodeAt:
+
+// charCodeAt returns utf-16 code of character allows only postive methods
+
+// let text ="bramhachari".charCodeAt(5)
+// console.log( text);
+
+// Slice:
+
+// slice returns the specific part of the string.
+// this method takes two parameters :the start index and the end index.
+
+// let str = "hi rajesh".slice(-6,-1);
+// console.log(str);
+
+// substring:
+
+// let str = "hello world";
+// // console.log(str.substring(0,5));
+// console.log(str.substring(-5,5));
+
+// let str = "hello world";
+// console.log(str.substring(1));
+// console.log(str.substring(-5,5));
+
+// substr:
+
+// let str = "hello world"
+// console.log(str.substr(5));
+
+// let str = "hello world"
+// console.log(str.substr(4));
+
+// let str = "hello world"
+// console.log(str.substr(0));
+
+// toUppercase&&toLowercase:
+
+
+// let str = "hello world";
+// console.log(str.toUpperCase()) //HELLO WORLD
+
+// let str = "HELLO WORLD";
+// console.log(str.toLowerCase()) //hello world
+
+// concat ():
+
+// let str = "hello world";
+// let str2="how are you";
+// console.log(str.concat(" ",str2)); hello world how are you.
+
+// trim ():
+
+// let str = "hello world"
+// console.log(str.trimStart()); 
+
+// repeat:
+
+// let str = "i love you sravanthi,"
+// console.log(str.repeat(5)); i love you sravanthi,i love you sravanthi,i love you sravanthi,i love you sravanthi,i love you sravanthi,
+
+// split:imp
+
+// let str = "sravanthi"
+// console.log(str.split("")); [
+//     's', 'r', 'a',
+//     'v', 'a', 'n',
+//     't', 'h', 'i'
+//   ]
+
+// let str = "sravanthi"
+// console.log(str.split(" ")); [ 'sravanthi' ]
+
+// let str = "sravanthi"
+// console.log(str.split("").reverse().join())
+
+// replace : imp
+
+// let str = "hello world, rajesh";
+// console.log(str.replace("world", "sravanthi")); 
+
+// repalce all: (dout)
+
+// let str = "hello world"
+// console.log(str.replace(/world/g, "sravanthi")); //hello sravanthi
+
+// indexof():
+
+// let str = "hello world"
+// console.log(str.indexOf("d"))
+
+// let str = "hello world"
+// console.log(str.indexOf("world"))
+
+// last indexOf:
+
+// let str = "hello world"
+// console.log(str.lastIndexOf("r"));
+
+// Search: (dout)
+
+// let str = "hello world"
+// console.log(str.search("hello"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
